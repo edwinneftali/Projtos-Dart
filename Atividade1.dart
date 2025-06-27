8b07f98a4a28b6367bdbe13d03c4b0306079d1dc
Edwin Neftaly <edwinneftali2006@gmail.com>
2025-06-27 09:16:57 -0300

    Create Atividade1.dart

---
+++ Atividade1.dart
@@ -0,0 +1,42 @@
+# Projtos-Dart
+Aqui estão alguns projetos básicos que fiz no Instituto Federal do Paraná.
+
+
+
+
+  import 'package:flutter/material.dart';
+
+class Exercicio1 extends StatelessWidget {
+  const Exercicio1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Exercicio 1'),
+        backgroundColor: colorScheme.inversePrimary,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            
+            Text(
+              'Bem-vindo!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, ),
+            ),
+
+            Text('Esta é uma tela simples contendo apenas Texto. Demostra como exibir informações estáticas usando um Stateless Widget. ',
+            textAlign: TextAlign.justify),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
